17df69ad9d3203a9a22b8e7883e6b988cd624543
Admin <admin@admins-macbook-pro.local>
2015-09-22 16:34:58 +0800

    Add Job model file

---
+++ Hands/Job.swift
@@ -0,0 +1,13 @@
+//
+//  Job.swift
+//  Hands
+//
+//  Created by William Yu on 9/22/15.
+//  Copyright © 2015 01. All rights reserved.
+//
+
+import Foundation
+
+class Job: NSObject {
+    
+}
---
+++ Hands/JobManager.swift
@@ -0,0 +1,15 @@
+//
+//  JobManager.swift
+//  Hands
+//
+//  Created by William Yu on 9/22/15.
+//  Copyright © 2015 01. All rights reserved.
+//
+
+import Foundation
+
+class JobManager: NSObject {
+    static let sharedManager = JobManager()
+    
+    
+}
